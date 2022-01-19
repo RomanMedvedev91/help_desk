9246ccf9fb728858750f28139631539cee3ac6ea
Nasser Mokadem <nmokadem@hotmail.com>
2022-01-19 04:22:20 +0000

    Use tinyMCE as a rich text editor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220117192957) do
+ActiveRecord::Schema.define(version: 20220119040618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20220117192957) do
     t.datetime "sla_valid_end_date"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.string   "reference"
   end
 
   add_index "contracts", ["service_level_agreement_id"], name: "index_contracts_on_service_level_agreement_id", using: :btree
